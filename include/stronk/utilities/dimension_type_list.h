c47fb85cf0f83d89d7d8664370d145f5e288cbe3
Anders Wind <anders.wind@twig.energy>
2024-07-10 11:19:23 +0200

    Good point

--- include/stronk/utilities/dimension_type_list.h
+++ include/stronk/utilities/dimension_type_list.h
@@ -8,6 +8,12 @@
 namespace twig::experiments
 {
 
+template<typename T>
+concept is_dimension = requires(T) {
+    typename T::unit_t;
+    T::rank;
+};
+
 template<typename UnitT, int16_t RankV>
 struct Dimension
 {
@@ -21,23 +27,45 @@ struct Dimension
     template<typename OtherDimT>
         requires std::same_as<UnitT, typename OtherDimT::unit_t>
     using subtract_t = Dimension<UnitT, RankV - OtherDimT::rank>;
+
+    using negate_t = Dimension<UnitT, -RankV>;
 };
 
-template<typename... Ts>
-struct DimensionList
-{
-    template<typename NewDimT>
-    using append_t = DimensionList<Ts..., NewDimT>;
+template<is_dimension... Ts>
+struct Dimensions;
 
-    template<typename... NewDimTs>
-    using concat_t = DimensionList<Ts..., NewDimTs...>;
+using EmptyDimensions = Dimensions<>;
+
+template<is_dimension... As>
+struct DimensionsMerge;
+
+// Empty Case implementation
+template<>
+struct DimensionsMerge<>
+{
+    template<typename AccumulatedDimensionListT, typename... Bs>
+    [[nodiscard]] constexpr static auto merge()
+    {
+        using res_t = typename AccumulatedDimensionListT::template concat_t<Bs...>;
+        return res_t();
+    }
+
+    template<typename... OtherTs>
+    using merge_t = decltype(merge<EmptyDimensions, OtherTs...>());
 };
 
-template<bool AddOrSubtractV, typename A, typename... As>
-struct DimensionListMerge
+template<is_dimension A, is_dimension... As>
+struct DimensionsMerge<A, As...>
 {
-    template<typename MergedDimensionTypeListT, typename B, typename... Bs>
-    [[nodiscard]] constexpr static auto merge(MergedDimensionTypeListT)
+    template<typename AccumulatedDimensionListT>
+    [[nodiscard]] constexpr static auto merge()
+    {
+        using res_t = typename AccumulatedDimensionListT::template concat_t<A, As...>;
+        return res_t();
+    }
+
+    template<typename AccumulatedDimensionListT, is_dimension B, is_dimension... Bs>
+    [[nodiscard]] constexpr static auto merge()
     {
         static_assert(A::rank != 0, "Cannot merge dimensions with rank 0");
         static_assert(B::rank != 0, "Cannot merge dimensions with rank 0");
@@ -48,71 +76,81 @@ struct DimensionListMerge
             ctti_type_index::type_id<typename A::unit_t>().before(ctti_type_index::type_id<typename B::unit_t>());
 
         if constexpr (t_equals_o) {
-            using new_dim = typename std::
-                conditional_t<AddOrSubtractV, typename A::template add_t<B>, typename A::template subtract_t<B>>;
+            using new_dim = typename A::template add_t<B>;
             if constexpr (new_dim::rank == 0) {
-                if constexpr (sizeof...(As) == 0) {
-                    using res_t = typename MergedDimensionTypeListT::template concat_t<Bs...>;
-                    return res_t();
-                } else if constexpr (sizeof...(Bs) == 0) {
-                    using res_t = typename MergedDimensionTypeListT::template concat_t<As...>;
-                    return res_t();
-                } else {
-                    return DimensionListMerge<AddOrSubtractV, As...>::template merge<MergedDimensionTypeListT, Bs...>(
-                        MergedDimensionTypeListT());
-                }
+                // The combination of the two dimensions results in a dimension with rank 0, so we can remove it
+                return DimensionsMerge<As...>::template merge<AccumulatedDimensionListT, Bs...>();
             } else {
-                using new_merged_list = MergedDimensionTypeListT::template append_t<new_dim>;
-                if constexpr (sizeof...(As) == 0) {
-                    using res_t = typename new_merged_list::template concat_t<Bs...>;
-                    return res_t();
-                } else if constexpr (sizeof...(Bs) == 0) {
-                    using res_t = typename new_merged_list::template concat_t<As...>;
-                    return res_t();
-                } else {
-                    return DimensionListMerge<AddOrSubtractV, As...>::template merge<new_merged_list, Bs...>(
-                        new_merged_list {});
-                }
+                using new_merged_list = AccumulatedDimensionListT::template push_back_t<new_dim>;
+                return DimensionsMerge<As...>::template merge<new_merged_list, Bs...>();
             }
         } else if constexpr (t_before_o) {
-            if constexpr (sizeof...(As) == 0) {
-                using res_t = typename MergedDimensionTypeListT::template concat_t<A, B, Bs...>;
-                return res_t();
-            } else {
-                using res_t = MergedDimensionTypeListT::template append_t<A>;
-                return DimensionListMerge<AddOrSubtractV, As...>::template merge<res_t, B, Bs...>(res_t());
-            }
+            using new_merged_list = AccumulatedDimensionListT::template push_back_t<A>;
+            return DimensionsMerge<As...>::template merge<new_merged_list, B, Bs...>();
         } else {
-            if constexpr (sizeof...(Bs) == 0) {
-                using res_t = typename MergedDimensionTypeListT::template concat_t<B, A, As...>;
-                return res_t();
-            } else {
-                using res_t = MergedDimensionTypeListT::template append_t<B>;
-                return DimensionListMerge<AddOrSubtractV, A, As...>::template merge<res_t, Bs...>(res_t());
-            }
+            using new_merged_list = AccumulatedDimensionListT::template push_back_t<B>;
+            return DimensionsMerge<A, As...>::template merge<new_merged_list, Bs...>();
         }
     }
 
-    template<typename... OtherTs>
-    using merge_t = decltype(merge<DimensionList<>, OtherTs...>(DimensionList<>()));
+    template<is_dimension... OtherTs>
+    using merge_t = decltype(merge<EmptyDimensions, OtherTs...>());
 };
 
-template<typename... As, typename... Bs>
-auto add_dimensions(DimensionList<As...>, DimensionList<Bs...>)
+template<is_dimension... Ts>
+struct Dimensions
 {
-    return DimensionListMerge<true, As...>::template merge<DimensionList<>, Bs...>(DimensionList<>());
+    template<is_dimension NewDimT>
+    using push_back_t = Dimensions<Ts..., NewDimT>;
+
+    template<is_dimension... NewDimTs>
+    using concat_t = Dimensions<Ts..., NewDimTs...>;
+
+    using negate_t = Dimensions<typename Ts::negate_t...>;
+
+    template<is_dimension... Bs>
+    static auto multiply(Dimensions<Bs...>)
+    {
+        return DimensionsMerge<Ts...>::template merge<Dimensions<>, Bs...>();
+    }
+    template<typename OtherDimensionsT>
+    using multiply_t = decltype(multiply(OtherDimensionsT {}));
+
+    template<is_dimension... Bs>
+    static auto divide(Dimensions<Bs...>)
+    {
+        using negated_b = Dimensions<Bs...>::negate_t;
+        return multiply(negated_b {});
+    }
+    template<typename OtherDimensionsT>
+    using divide_t = decltype(divide(OtherDimensionsT {}));
+};
+
+namespace
+{
+
+template<typename... ExistingDimTs>
+auto create(Dimensions<ExistingDimTs...> dim)
+{
+    return dim;
 }
 
-template<typename ListA, typename ListB>
-using add_dimensions_t = decltype(add_dimensions(ListA {}, ListB {}));
-
-template<typename... As, typename... Bs>
-auto subtract_dimensions(DimensionList<As...>, DimensionList<Bs...>)
+template<typename... ExistingDimTs, is_dimension Dim, is_dimension... DimTs>
+auto create(Dimensions<ExistingDimTs...> dims, Dim, DimTs... rest)
 {
-    return DimensionListMerge<false, As...>::template merge<DimensionList<>, Bs...>(DimensionList<>());
+    return create(dims.multiply(Dimensions<Dim> {}), rest...);
 }
 
-template<typename ListA, typename ListB>
-using subtract_dimensions_t = decltype(subtract_dimensions(ListA {}, ListB {}));
+}  // namespace
+
+// Ensures order and uniqueness of dimensions
+template<is_dimension... DimTs>
+auto create_dimensions(DimTs... dims)
+{
+    return create(EmptyDimensions {}, dims...);
+}
+
+template<is_dimension... DimTs>
+using create_dimensions_t = decltype(create_dimensions(DimTs {}...));
 
 }  // namespace twig::experiments
--- tests/src/utilities/dimension_type_list_tests.cpp
+++ tests/src/utilities/dimension_type_list_tests.cpp
@@ -12,9 +12,39 @@ struct distance
 struct time
 {};
 
-using Speed = DimensionList<Dimension<distance, 1>, Dimension<time, -1>>;
+using Distance = Dimensions<Dimension<distance, 1>>;
+using Time = Dimensions<Dimension<time, 1>>;
+using Speed = Dimensions<Dimension<distance, 1>, Dimension<time, -1>>;
 
-static_assert(std::same_as<add_dimensions_t<Speed, Speed>, DimensionList<Dimension<distance, 2>, Dimension<time, -2>>>);
-static_assert(std::same_as<subtract_dimensions_t<Speed, Speed>, DimensionList<>>);
+static_assert(std::same_as<create_dimensions_t<>, EmptyDimensions>);
+static_assert(std::same_as<create_dimensions_t<Dimension<distance, 1>>, Distance>);
+static_assert(std::same_as<create_dimensions_t<Dimension<distance, 1>, Dimension<time, -1>>, Speed>);
+static_assert(std::same_as<create_dimensions_t<Dimension<distance, 1>, Dimension<time, -1>>,
+                           create_dimensions_t<Dimension<time, -1>, Dimension<distance, 1>>>);
+static_assert(std::same_as<create_dimensions_t<Dimension<distance, 1>, Dimension<distance, 1>>,
+                           create_dimensions_t<Dimension<distance, 2>>>);
+
+// multiply
+static_assert(std::same_as<EmptyDimensions::multiply_t<EmptyDimensions>, EmptyDimensions>);
+static_assert(std::same_as<Distance::multiply_t<Distance::negate_t>, EmptyDimensions>);
+static_assert(std::same_as<Distance::multiply_t<Distance>, Dimensions<Dimension<distance, 2>>>);
+static_assert(std::same_as<Distance::multiply_t<Distance>::multiply_t<Distance>, Dimensions<Dimension<distance, 3>>>);
+static_assert(std::same_as<Distance::multiply_t<Time>, Dimensions<Dimension<distance, 1>, Dimension<time, 1>>>);
+static_assert(std::same_as<Distance::multiply_t<Time>, Time::multiply_t<Distance>>);
+static_assert(std::same_as<Distance::multiply_t<Time::negate_t>, Speed>);
+static_assert(std::same_as<Time::negate_t::multiply_t<Distance>, Speed>);
+static_assert(std::same_as<Distance::multiply_t<Time>::multiply_t<Distance>,
+                           Dimensions<Dimension<distance, 2>, Dimension<time, 1>>>);
+static_assert(std::same_as<Distance::negate_t::multiply_t<Time>::multiply_t<Distance>, Dimensions<Dimension<time, 1>>>);
+static_assert(std::same_as<Speed::multiply_t<Speed>, Dimensions<Dimension<distance, 2>, Dimension<time, -2>>>);
+static_assert(std::same_as<Speed::multiply_t<Speed::negate_t>, EmptyDimensions>);
+
+// Divide
+static_assert(std::same_as<EmptyDimensions::divide_t<EmptyDimensions>, EmptyDimensions>);
+static_assert(std::same_as<Distance::divide_t<Distance>, EmptyDimensions>);
+static_assert(std::same_as<Distance::divide_t<Distance>::divide_t<Distance>, Dimensions<Dimension<distance, -1>>>);
+static_assert(std::same_as<Speed::divide_t<Speed>, EmptyDimensions>);
+static_assert(std::same_as<Distance::divide_t<Time>, Speed>);
+static_assert(std::same_as<Time::divide_t<Distance>, Dimensions<Dimension<distance, -1>, Dimension<time, 1>>>);
 
 }  // namespace twig::experiments
