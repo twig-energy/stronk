faba35f9358909a4dfc958ce05793d5fc8e6ad49
Anders Wind <anders.wind@twig.energy>
2024-08-13 16:19:08 +0200

    Fix implementation

--- tests/src/extensions/glaze_tests.cpp
+++ tests/src/extensions/glaze_tests.cpp
@@ -20,6 +20,11 @@ struct an_int_can_glaze_de_and_serialize_wrapper
     an_int_can_glaze_de_and_serialize number;
 
     auto operator==(const an_int_can_glaze_de_and_serialize_wrapper& other) const -> bool = default;
+
+    struct glaze
+    {
+        constexpr static auto value = glz::object(&an_int_can_glaze_de_and_serialize_wrapper::number);
+    };
 };
 
 TEST(can_glaze_de_and_serialize, when_serializing_stronk_integer_then_can_deserialize_to_same_value)
@@ -42,14 +47,44 @@ struct a_string_can_glaze_de_and_serialize_wrapper
     a_string_can_glaze_de_and_serialize str;
 
     auto operator==(const a_string_can_glaze_de_and_serialize_wrapper& other) const -> bool = default;
+
+    struct glaze
+    {
+        constexpr static auto value = glz::object(&a_string_can_glaze_de_and_serialize_wrapper::str);
+    };
 };
 
 TEST(can_glaze_de_and_serialize, when_serializing_stronk_string_then_can_deserialize_to_same_value)
 {
     const auto val = a_string_can_glaze_de_and_serialize_wrapper {.str = a_string_can_glaze_de_and_serialize {"hello"}};
     auto json_str = glz::write_json(val);
-    EXPECT_EQ(R"("{"str":"hello"}")", json_str);
+    EXPECT_EQ(R"({"str":"hello"})", json_str);
     EXPECT_EQ(val, glz::read_json<a_string_can_glaze_de_and_serialize_wrapper>(json_str).value());
+}
+
+struct a_class_with_two_can_glaze_members
+{
+    an_int_can_glaze_de_and_serialize val1;
+    a_string_can_glaze_de_and_serialize val2;
+
+    auto operator==(const a_class_with_two_can_glaze_members& other) const -> bool = default;
+
+    struct glaze
+    {
+        constexpr static auto value =
+            glz::object(&a_class_with_two_can_glaze_members::val1, &a_class_with_two_can_glaze_members::val2);
+    };
+};
+
+TEST(can_glaze_de_and_serialize, when_serializing_a_type_with_multiple_members_it_roundtrips)
+{
+    const auto val = a_class_with_two_can_glaze_members {
+        .val1 = an_int_can_glaze_de_and_serialize {24},
+        .val2 = a_string_can_glaze_de_and_serialize {"world"},
+    };
+    auto json_str = glz::write_json(val);
+    EXPECT_EQ(R"({"val1":24,"val2":"world"})", json_str);
+    EXPECT_EQ(val, glz::read_json<a_class_with_two_can_glaze_members>(json_str).value());
 }
 
 }  // namespace twig
