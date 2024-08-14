a83a25fc9464d1cdb1895d876eff559ab8a4da15
Anders Wind <anders.wind@twig.energy>
2024-08-14 09:08:16 +0200

    Adding constexpr to allow constexpr format

--- include/stronk/can_format.h
+++ include/stronk/can_format.h
@@ -6,7 +6,7 @@
 template<twig::stronk_like StronkT>
 struct std::formatter<StronkT> : std::formatter<typename StronkT::underlying_type>
 {
-    auto format(const StronkT& val, std::format_context& context) const
+    constexpr auto format(const StronkT& val, std::format_context& context) const
     {
         return std::formatter<typename StronkT::underlying_type>::format(val.template unwrap<StronkT>(), context);
     }
