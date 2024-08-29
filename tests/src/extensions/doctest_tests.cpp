f9f218bbcda40574deb433dd74542d6c8aac1055
Anders Wind <anders.wind@twig.energy>
2024-08-29 11:34:09 +0200

    second case

--- tests/src/extensions/doctest_tests.cpp
+++ tests/src/extensions/doctest_tests.cpp
@@ -13,9 +13,17 @@ struct a_type_to_doctest_print : stronk<a_type_to_doctest_print, int>
     using stronk::stronk;
 };
 
+struct a_recursive_type_to_doctest_print : stronk<a_recursive_type_to_doctest_print, a_type_to_doctest_print>
+{
+    using stronk::stronk;
+};
+
 TEST(can_doctest_extend, when_given_a_stronk_type_doctest_can_convert_the_underlying_string)  // NOLINT
 {
     EXPECT_EQ(doctest::StringMaker<a_type_to_doctest_print>::convert(a_type_to_doctest_print {5}), "5");
+    EXPECT_EQ(doctest::StringMaker<a_recursive_type_to_doctest_print>::convert(
+                  a_recursive_type_to_doctest_print {a_type_to_doctest_print {42}}),
+              "42");
 }
 
 }  // namespace twig
