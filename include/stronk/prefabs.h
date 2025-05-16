e1f036b7d9ece97ff43b110f646560e0fcdf9fa7
Anders Wind <anders.wind@twig.energy>
2025-05-16 17:12:54 +0000

    remove old unit and fixed specializer code

--- include/stronk/prefabs.h
+++ include/stronk/prefabs.h
@@ -1,34 +1,8 @@
 #pragma once
 #include <stronk/stronk.h>
-#include <stronk/unit.h>
 
 namespace twig
 {
-
-template<typename Tag, typename T, template<typename> typename... Skills>
-struct stronk_default_unit
-    : stronk<Tag,
-             T,
-             unit,
-             can_add,
-             can_subtract,
-             can_negate,
-             can_order,
-             transform_skill,
-             default_can_equate_builder<T>::template skill,
-             Skills...>
-{
-    using stronk<Tag,
-                 T,
-                 unit,
-                 can_add,
-                 can_subtract,
-                 can_negate,
-                 can_order,
-                 transform_skill,
-                 default_can_equate_builder<T>::template skill,
-                 Skills...>::stronk;
-};
 
 template<typename Tag, template<typename> typename... Skills>
 struct stronk_flag : stronk<Tag, bool, can_be_used_as_flag, can_order, can_equate, Skills...>
