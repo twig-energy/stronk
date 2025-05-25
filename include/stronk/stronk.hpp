0da9cd6db20e8cd9cbcfa2c0ca8c40198daaf3d8
Anders Wind <anders.wind@twig.energy>
2025-05-25 01:11:55 +0000

    Move headers around

--- include/stronk/stronk.hpp
+++ include/stronk/stronk.hpp
@@ -321,13 +321,3 @@ struct can_size
 };
 
 }  // namespace twig
-
-// TODO(anders.wind) remove
-// here because we want to be backwards compatible, fix when downstream has been updated to import these directly
-#include "stronk/skills/can_abs.hpp"              // IWYU pragma: keep
-#include "stronk/skills/can_be_used_as_flag.hpp"  // IWYU pragma: keep
-#include "stronk/skills/can_divide.hpp"           // IWYU pragma: keep
-#include "stronk/skills/can_index.hpp"            // IWYU pragma: keep
-#include "stronk/skills/can_isnan.hpp"            // IWYU pragma: keep
-#include "stronk/skills/can_iterate.hpp"          // IWYU pragma: keep
-#include "stronk/skills/can_multiply.hpp"         // IWYU pragma: keep
