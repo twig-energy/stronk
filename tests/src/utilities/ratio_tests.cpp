e2cf42aaac0d25af30428722d8c60aa6bd839f95
Anders Wind <anders.wind@twig.energy>
2025-06-04 16:57:48 +0000

    gcd test

--- tests/src/utilities/ratio_tests.cpp
+++ tests/src/utilities/ratio_tests.cpp
@@ -2,6 +2,8 @@
 
 #include "stronk/utilities/ratio.hpp"
 
+#include <gtest/gtest.h>
+
 namespace twig
 {
 
@@ -9,5 +11,15 @@ namespace twig
 static_assert(std::same_as<ratio_multiply<ratio<1'000'000'000'000>, ratio<1'000'000'000'000>>,
                            ratio<stronk_details::u_biggest_int_t {1'000'000'000'000}
                                  * stronk_details::u_biggest_int_t {1'000'000'000'000}>>);
+
+TEST(gcd, few_examples)
+{
+    EXPECT_EQ(stronk_details::gcd(0, 0), 0);
+    EXPECT_EQ(stronk_details::gcd(1, 0), 1);
+    EXPECT_EQ(stronk_details::gcd(0, 1), 1);
+    EXPECT_EQ(stronk_details::gcd(2, 4), 2);
+    EXPECT_EQ(stronk_details::gcd(4, 2), 2);
+    EXPECT_EQ(stronk_details::gcd(12, 15), 3);
+}
 
 }  // namespace twig
