04faf2928c8766f315995b1c1265904256311f6d
Anders Wind <anders.wind@twig.energy>
2025-06-05 09:27:39 +0000

    added a stronk_string prefab

---
+++ tests/src/prefabs/stronk_string_tests.cpp
@@ -0,0 +1,26 @@
+
+
+#include "stronk/prefabs/stronk_string.hpp"
+
+#include <gtest/gtest.h>
+
+#include "stronk/stronk.hpp"
+
+namespace twig
+{
+
+struct a_string_type : stronk_string<a_string_type>
+{
+    using stronk::stronk;
+};
+
+static_assert(a_string_type {"hello"}.size() == 5);
+
+TEST(a_string_type, can_be_used_as_strings)
+{
+    auto stronk_string = a_string_type {"hello"};
+    EXPECT_EQ(stronk_string.size(), 5);
+    EXPECT_EQ(stronk_string, a_string_type {"hello"});
+}
+
+}  // namespace twig
