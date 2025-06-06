1d0b00bd6c2f70105bae0e64d066e2103345e3fd
Anders Wind <anders.wind@twig.energy>
2025-06-06 10:35:43 +0000

    Add stronk inline more places + return of the init list

--- tests/src/prefabs/stronk_vector_tests.cpp
+++ tests/src/prefabs/stronk_vector_tests.cpp
@@ -18,8 +18,8 @@ struct a_vector_type : stronk_vector<a_vector_type, int>
 
 TEST(stronk_vector, can_be_used_as_vector)
 {
-    auto stronk_vector = a_vector_type {std::vector {1, 2, 4}};
-    auto another = a_vector_type {std::vector {1, 2, 3}};
+    auto stronk_vector = a_vector_type {1, 2, 4};
+    auto another = a_vector_type {1, 2, 3};
     EXPECT_EQ(stronk_vector.size(), 3);
     EXPECT_EQ(stronk_vector, stronk_vector);
     EXPECT_NE(stronk_vector, another);
