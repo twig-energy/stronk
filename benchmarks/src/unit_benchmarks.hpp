3b9d42f9b872b16e16e185140910a0f19d87d15c
Anders Wind <anders.wind@twig.energy>
2025-06-26 19:20:41 +0000

    fix divide

--- benchmarks/src/unit_benchmarks.hpp
+++ benchmarks/src/unit_benchmarks.hpp
@@ -5,184 +5,108 @@
 #include <iostream>
 #include <vector>
 
+#include <fmt/format.h>
 #include <nanobench.h>
 
 #include "./benchmark_helpers.hpp"
 
-template<typename T>
-void benchmark_add_units(ankerl::nanobench::Bench& bench, size_t size)
-{
-    auto vec_a = std::vector<T>(size);
-    auto vec_b = std::vector<T>(size);
-    std::ranges::generate(vec_a, []() { return generate_randomish<T> {}(); });
-    std::ranges::generate(vec_b, []() { return generate_randomish<T> {}(); });
-
-    bench.complexityN(size).run(get_name<T>(),
-                                [&]()
-                                {
-                                    for (auto i = 0ULL; i < size; i++) {
-                                        T res;
-                                        ankerl::nanobench::doNotOptimizeAway(res);
-                                        res = vec_a[i] + vec_b[i];  // NOLINT
-                                        ankerl::nanobench::doNotOptimizeAway(res);
-                                    }
-                                });
-}
-
-template<typename T, size_t WidthV>
-void benchmark_add_units_simd(ankerl::nanobench::Bench& bench, size_t size)
-{
-    auto vec_a = std::vector<T>(size);
-    auto vec_b = std::vector<T>(size);
-    std::generate(vec_a.begin(), vec_a.end(), []() { return generate_randomish<T> {}(); });
-    std::generate(vec_b.begin(), vec_b.end(), []() { return generate_randomish<T> {}(); });
-
-    bench.complexityN(size).run(get_name<T>() + "_simd",
-                                [&]()
-                                {
-                                    for (auto i = 0ULL; i < size - WidthV; i++) {
-                                        auto array_c = std::array<T, WidthV> {};
-                                        ankerl::nanobench::doNotOptimizeAway(array_c.data());
-                                        // We expect the inner loop to be vectorized to SIMD instructions
-                                        for (size_t j = 0; j < WidthV; j++) {
-                                            array_c[j] = vec_a[i + j] + vec_b[i + j];  // NOLINT
-                                        }
-                                        ankerl::nanobench::doNotOptimizeAway(array_c);
-                                    }
-                                });
-}
-
-template<typename T>
-void benchmark_subtract_units(ankerl::nanobench::Bench& bench, size_t size)
-{
-    auto vec_a = std::vector<T>(size);
-    auto vec_b = std::vector<T>(size);
-    std::ranges::generate(vec_a, []() { return generate_randomish<T> {}(); });
-    std::ranges::generate(vec_b, []() { return generate_randomish<T> {}(); });
-
-    bench.complexityN(size).run(get_name<T>(),
-                                [&]()
-                                {
-                                    for (auto i = 0ULL; i < size; i++) {
-                                        T res;
-                                        ankerl::nanobench::doNotOptimizeAway(res);
-                                        res = vec_a[i] - vec_b[i];  // NOLINT
-                                        ankerl::nanobench::doNotOptimizeAway(res);
-                                    }
-                                });
-}
-
-template<typename T, size_t WidthV>
-void benchmark_subtract_units_simd(ankerl::nanobench::Bench& bench, size_t size)
-{
-    auto vec_a = std::vector<T>(size);
-    auto vec_b = std::vector<T>(size);
-    std::ranges::generate(vec_a, []() { return generate_randomish<T> {}(); });
-    std::ranges::generate(vec_b, []() { return generate_randomish<T> {}(); });
-
-    bench.complexityN(size).run(get_name<T>() + "_simd",
-                                [&]()
-                                {
-                                    for (auto i = 0ULL; i < size - WidthV; i++) {
-                                        auto array_c = std::array<T, WidthV> {};
-                                        ankerl::nanobench::doNotOptimizeAway(array_c.data());
-                                        // We expect the inner loop to be vectorized to SIMD instructions
-                                        for (size_t j = 0; j < WidthV; j++) {
-                                            array_c[j] = vec_a[i + j] - vec_b[i + j];  // NOLINT
-                                        }
-                                        ankerl::nanobench::doNotOptimizeAway(array_c);
-                                    }
-                                });
-}
-
 template<typename T, typename O>
-void benchmark_multiply_units(ankerl::nanobench::Bench& bench, size_t size)
+constexpr void benchmark_units_operation(ankerl::nanobench::Bench& bench,
+                                         size_t size,
+                                         const auto& op,
+                                         O o_min_val = O {})
 {
     auto vec_a = std::vector<T>(size);
     auto vec_b = std::vector<O>(size);
     std::ranges::generate(vec_a, []() { return generate_randomish<T> {}(); });
-    std::ranges::generate(vec_b, []() { return generate_randomish<O> {}(); });
-    using res_t = decltype(T {} * O {});
+    std::ranges::generate(vec_b, [&o_min_val]() { return generate_randomish<O> {}() + o_min_val; });
 
-    bench.complexityN(size).run(get_name<T>() + "_" + get_name<O>(),
+    bench.complexityN(size).run(fmt::format("{}_{}", get_name<T>(), get_name<O>()),
                                 [&]()
                                 {
+                                    using ResT = decltype(op(T {}, O {}));
                                     for (auto i = 0ULL; i < size; i++) {
-                                        res_t res;
+                                        ResT res;
                                         ankerl::nanobench::doNotOptimizeAway(res);
-                                        res = vec_a[i] * vec_b[i];  // NOLINT
+                                        res = op(vec_a[i], vec_b[i]);  // NOLINT
                                         ankerl::nanobench::doNotOptimizeAway(res);
                                     }
                                 });
 }
 
 template<typename T, typename O, size_t WidthV>
-void benchmark_multiply_units_simd(ankerl::nanobench::Bench& bench, size_t size)
+constexpr void benchmark_units_simd_operation(ankerl::nanobench::Bench& bench,
+                                              size_t size,
+                                              const auto& op,
+                                              O o_min_val = O {})
 {
     auto vec_a = std::vector<T>(size);
     auto vec_b = std::vector<O>(size);
     std::ranges::generate(vec_a, []() { return generate_randomish<T> {}(); });
-    std::ranges::generate(vec_b, []() { return generate_randomish<O> {}(); });
-    using res_t = decltype(T {} * O {});
+    std::ranges::generate(vec_b, [&o_min_val]() { return generate_randomish<O> {}() + o_min_val; });
 
-    bench.complexityN(size).run(get_name<T>() + "_" + get_name<O>() + "_simd",
+    bench.complexityN(size).run(fmt::format("{}_{}", get_name<T>(), get_name<O>()),
                                 [&]()
                                 {
+                                    using ResT = decltype(op(T {}, O {}));
                                     for (auto i = 0ULL; i < size - WidthV; i++) {
-                                        auto array_c = std::array<res_t, WidthV> {};
+                                        auto array_c = std::array<ResT, WidthV> {};
                                         ankerl::nanobench::doNotOptimizeAway(array_c.data());
                                         // We expect the inner loop to be vectorized to SIMD instructions
                                         for (size_t j = 0; j < WidthV; j++) {
-                                            array_c[j] = vec_a[i + j] * vec_b[i + j];  // NOLINT
+                                            array_c[j] = op(vec_a[i + j], vec_b[i + j]);  // NOLINT
                                         }
                                         ankerl::nanobench::doNotOptimizeAway(array_c);
                                     }
                                 });
+}
+
+template<typename T>
+constexpr void benchmark_add_units(ankerl::nanobench::Bench& bench, size_t size)
+{
+    benchmark_units_operation<T, T>(bench, size, std::plus<> {});
+}
+
+template<typename T, size_t WidthV>
+constexpr void benchmark_add_units_simd(ankerl::nanobench::Bench& bench, size_t size)
+{
+    benchmark_units_simd_operation<T, T, WidthV>(bench, size, std::plus<> {});
+}
+
+template<typename T>
+constexpr void benchmark_subtract_units(ankerl::nanobench::Bench& bench, size_t size)
+{
+    benchmark_units_operation<T, T>(bench, size, std::minus<> {});
+}
+
+template<typename T, size_t WidthV>
+constexpr void benchmark_subtract_units_simd(ankerl::nanobench::Bench& bench, size_t size)
+{
+    benchmark_units_simd_operation<T, T, WidthV>(bench, size, std::minus<> {});
 }
 
 template<typename T, typename O>
-void benchmark_divide_units(ankerl::nanobench::Bench& bench, size_t size)
+constexpr void benchmark_multiply_units(ankerl::nanobench::Bench& bench, size_t size)
 {
-    auto vec_a = std::vector<T>(size);
-    auto vec_b = std::vector<O>(size);
-    std::ranges::generate(vec_a, []() { return generate_randomish<T> {}(); });
-    std::ranges::generate(vec_b, []() { return generate_none_zero_randomish<O>(); });
-    using res_t = decltype(T {} / O {});
-
-    bench.complexityN(size).run(get_name<T>() + "_" + get_name<O>(),
-                                [&]()
-                                {
-                                    for (auto i = 0ULL; i < size; i++) {
-                                        res_t res;
-                                        ankerl::nanobench::doNotOptimizeAway(res);
-                                        res = vec_a[i] / vec_b[i];  // NOLINT
-                                        ankerl::nanobench::doNotOptimizeAway(res);
-                                    }
-                                });
+    benchmark_units_operation<T, O>(bench, size, [](auto x, auto y) { return x * y; });  // NOLINT
 }
 
 template<typename T, typename O, size_t WidthV>
-void benchmark_divide_units_simd(ankerl::nanobench::Bench& bench, size_t size)
+constexpr void benchmark_multiply_units_simd(ankerl::nanobench::Bench& bench, size_t size)
 {
-    auto vec_a = std::vector<T>(size);
-    auto vec_b = std::vector<O>(size);
-    std::ranges::generate(vec_a, []() { return generate_randomish<T> {}(); });
-    std::ranges::generate(vec_b, []() { return generate_none_zero_randomish<O>(); });
-    using res_t = decltype(T {} / O {});
+    benchmark_units_simd_operation<T, O, WidthV>(bench, size, [](auto x, auto y) { return x * y; });  // NOLINT
+}
 
-    bench.complexityN(size).run(get_name<T>() + "_" + get_name<O>() + "_simd",
-                                [&]()
-                                {
-                                    for (auto i = 0ULL; i < size - WidthV; i++) {
-                                        auto array_c = std::array<res_t, WidthV> {};
-                                        ankerl::nanobench::doNotOptimizeAway(array_c.data());
-                                        // We expect the inner loop to be vectorized to SIMD instructions
-                                        for (size_t j = 0; j < WidthV; j++) {
-                                            array_c[j] = vec_a[i + j] / vec_b[i + j];  // NOLINT
-                                        }
-                                        ankerl::nanobench::doNotOptimizeAway(array_c);
-                                    }
-                                });
+template<typename T, typename O>
+constexpr void benchmark_divide_units(ankerl::nanobench::Bench& bench, size_t size)
+{
+    benchmark_units_operation<T, O>(bench, size, [](auto x, auto y) { return x / y; }, O {1});  // NOLINT
+}
+
+template<typename T, typename O, size_t WidthV>
+constexpr void benchmark_divide_units_simd(ankerl::nanobench::Bench& bench, size_t size)
+{
+    benchmark_units_simd_operation<T, O, WidthV>(bench, size, [](auto x, auto y) { return x / y; }, O {1});  // NOLINT
 }
 
 inline void run_add_units_benchmarks()
