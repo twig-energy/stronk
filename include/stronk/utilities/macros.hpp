471ddb0815e9b59742dd12492798f89888d7e62a
Anders Wind <anders.wind@twig.energy>
2026-01-26 12:59:06 +0000

    prefix STRONK before macro

--- include/stronk/utilities/macros.hpp
+++ include/stronk/utilities/macros.hpp
@@ -1,15 +1,15 @@
 #pragma once
 
 #if defined(_MSC_VER)
-#    define EMPTY_BASES __declspec(empty_bases)
+#    define STRONK_EMPTY_BASES __declspec(empty_bases)
 #    define STRONK_FORCEINLINE __forceinline  // NOLINT
 #elif defined(__clang__)
-#    define EMPTY_BASES
+#    define STRONK_EMPTY_BASES
 #    define STRONK_FORCEINLINE __attribute__((always_inline))  // NOLINT
 #elif defined(__GNUC__)
-#    define EMPTY_BASES
+#    define STRONK_EMPTY_BASES
 #    define STRONK_FORCEINLINE __attribute__((always_inline))  // NOLINT
 #else
-#    define EMPTY_BASES
+#    define STRONK_EMPTY_BASES
 #    define STRONK_FORCEINLINE  // NOLINT
 #endif
