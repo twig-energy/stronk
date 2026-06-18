bb653d47d37bfd2ab0c5c559736c021b96776396
Anders Wind <anders.wind@twig.energy>
2026-06-18 17:43:09 +0200

    Fix Version setup and install rules (#77)
    ### Why 
    The Fileset was not up to date - and it was overridden in the install
    setup
    
    ### What was changed
    - Fixed the header fileset.
    - Fixed CI not calling the file set include checker
    - Fixed the cmake install script
    - Moved version away from version.py and just kept it in CMakeLists.txt

--- cmake/install-rules.cmake
+++ cmake/install-rules.cmake
@@ -1,12 +1,4 @@
-# Project is configured with no languages, so tell GNUInstallDirs the lib dir
-set(CMAKE_INSTALL_LIBDIR
-    lib
-    CACHE PATH ""
-)
-
-include(CMakePackageConfigHelpers)
-include(GNUInstallDirs)
-
+# Install headers under: <prefix>/include/stronk-<version>/
 if (PROJECT_IS_TOP_LEVEL)
     set(CMAKE_INSTALL_INCLUDEDIR
         "include/stronk-${PROJECT_VERSION}"
@@ -14,32 +6,34 @@ if (PROJECT_IS_TOP_LEVEL)
     )
 endif ()
 
-# find_package(<package>) call for consumers to find this project
+include(GNUInstallDirs)
+include(CMakePackageConfigHelpers)
+
+# find_package(stronk)
 set(package stronk)
 
-install(
-    DIRECTORY include/
-    DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}"
-    COMPONENT stronk_Development
+# Generate version file
+write_basic_package_version_file("${PROJECT_BINARY_DIR}/${package}ConfigVersion.cmake" COMPATIBILITY SameMajorVersion ARCH_INDEPENDENT)
+
+# Allow package maintainers to override config location
+set(stronk_INSTALL_CMAKEDIR
+    "${CMAKE_INSTALL_DATADIR}/${package}"
+    CACHE PATH "CMake package config location relative to the install prefix"
 )
 
+mark_as_advanced(stronk_INSTALL_CMAKEDIR)
+
+# Install target + exported usage requirements
 install(
     TARGETS twig_stronk
     EXPORT stronkTargets
+    ARCHIVE COMPONENT twig-eex_development
     INCLUDES
     DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}"
     FILE_SET HEADERS
 )
 
-write_basic_package_version_file("${package}ConfigVersion.cmake" COMPATIBILITY SameMajorVersion ARCH_INDEPENDENT)
-
-# Allow package maintainers to freely override the path for the configs
-set(stronk_INSTALL_CMAKEDIR
-    "${CMAKE_INSTALL_DATADIR}/${package}"
-    CACHE PATH "CMake package config location relative to the install prefix"
-)
-mark_as_advanced(stronk_INSTALL_CMAKEDIR)
-
+# Install package config
 install(
     FILES cmake/install-config.cmake
     DESTINATION "${stronk_INSTALL_CMAKEDIR}"
@@ -47,14 +41,17 @@ install(
     COMPONENT stronk_Development
 )
 
+# Install package version file
 install(
     FILES "${PROJECT_BINARY_DIR}/${package}ConfigVersion.cmake"
     DESTINATION "${stronk_INSTALL_CMAKEDIR}"
     COMPONENT stronk_Development
 )
 
+# Install exported targets
 install(
     EXPORT stronkTargets
+    FILE stronkTargets.cmake
     NAMESPACE twig::
     DESTINATION "${stronk_INSTALL_CMAKEDIR}"
     COMPONENT stronk_Development
