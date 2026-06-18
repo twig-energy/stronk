ecb3b19c53e587ac58cb45a061c5fe12a14d2c03
Anders Wind <anders.wind@twig.energy>
2026-06-18 16:19:45 +0000

    better readme

--- examples/firstname_lastname_example.cpp
+++ examples/firstname_lastname_example.cpp
@@ -4,11 +4,11 @@
 #include <stronk/skills/can_stream.hpp>
 #include <stronk/stronk.hpp>
 
-struct FirstName : twig::stronk<FirstName, std::string, twig::can_ostream>
+struct FirstName : twig::stronk<FirstName, std::string>
 {
     using stronk::stronk;
 };
-struct LastName : twig::stronk<LastName, std::string>
+struct LastName : twig::stronk<LastName, std::string, twig::can_ostream>
 {
     using stronk::stronk;
 };
@@ -16,10 +16,11 @@ struct LastName : twig::stronk<LastName, std::string>
 // Strong types protects you from accidentally passing the wrong argument to the wrong parameter.
 void print_name(const LastName& lastname, const FirstName& firstname)
 {
-    // The twig::can_ostream skill overloads the `operator<<(ostream&)` for your type.
-    std::cout << firstname << " ";
-    // You can also access the underlying type by using the .unwrap<Type>() function.
+    // You can access the underlying type by using the .unwrap<Type>() function.
+    std::cout << firstname.unwrap<FirstName>() << " ";
+    // Adding the twig::can_ostream skill overloads the `operator<<(ostream&)` for your type.
     std::cout << lastname.unwrap<LastName>() << std::endl;
+    // Generally you should prefer skills or custom functions over unwrapping to preserve your strong types
 }
 
 auto main() -> int
